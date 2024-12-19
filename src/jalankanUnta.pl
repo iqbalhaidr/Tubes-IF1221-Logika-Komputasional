9e3b9d9c217646898f60e0f436612b8d9055f99c
Hanif Kalyana Aditya <19623120@std.stei.itb.ac.id>
2024-12-20 06:21:03 +0700

    feat : tukarUnta

--- src/jalankanUnta.pl
+++ src/jalankanUnta.pl
@@ -32,6 +32,28 @@ jalankanUnta :-
     format('Unta ~w berpindah ke petak ~d~n', [Warna, PosisiBaru]),
     nextTurn. 
 
+/* Tukar Unta */
+tukarUnta :-
+    kocokDadu(Warna1, _), % Gunakan kocokDadu untuk memilih warna pertama
+    kocokDadu(Warna2, _), % Gunakan kocokDadu untuk memilih warna kedua
+    Warna1 \= Warna2, % Pastikan dua warna tidak sama
+    unta(Warna1, Posisi1, Tumpuk1),
+    unta(Warna2, Posisi2, Tumpuk2),
+    format('Kocok dadu...\nWarna unta 1: ~w\nWarna unta 2: ~w\n', [Warna1, Warna2]),
+    format('Posisi sebelum unta ~w: ~d\n', [Warna1, Posisi1]),
+    format('Posisi sebelum unta ~w: ~d\n', [Warna2, Posisi2]),
+
+    % Tukar posisi dan tumpukan unta
+    retract(unta(Warna1, Posisi1, Tumpuk1)),
+    retract(unta(Warna2, Posisi2, Tumpuk2)),
+    asserta(unta(Warna1, Posisi2, Tumpuk1)),
+    asserta(unta(Warna2, Posisi1, Tumpuk2)),
+
+    format('Unta ~w dan ~w bertukar posisi...\n', [Warna1, Warna2]),
+    format('Posisi sesudah unta ~w: ~d\n', [Warna1, Posisi2]),
+    format('Posisi sesudah unta ~w: ~d\n', [Warna2, Posisi1]),
+    nextTurn.
+
 /* Fungsi untuk melanjutkan ke giliran berikutnya */
 nextTurn :-
     jumlahPemain(Jumlah),
